2552c56f8b9a233060b7c61740d75d89255dadc5
Hanpita <52304634+Hanpita@users.noreply.github.com>
2019-08-16 09:05:17 +0800

    Add files via upload

--- js/Untitled-1-js.js
+++ js/Untitled-1-js.js
@@ -15,6 +15,12 @@ $(document).ready(function () {
         var li3='A';
         var li4='A';
         var li5='C';
+		var li6='A';
+		var li7='B';
+		var li8='A';
+		var li9='B';
+		var li10='A';
+		
 
 
         var choice1=$('input:radio:checked').eq(0).val();
@@ -22,27 +28,52 @@ $(document).ready(function () {
         var choice3=$('input:radio:checked').eq(2).val();
         var choice4=$('input:radio:checked').eq(3).val();
         var choice5=$('input:radio:checked').eq(4).val();
+		var choice6=$('input:radio:checked').eq(5).val();
+		var choice7=$('input:radio:checked').eq(6).val();
+		var choice8=$('input:radio:checked').eq(7).val();
+		var choice9=$('input:radio:checked').eq(8).val();
+		var choice10=$('input:radio:checked').eq(9).val();
 
 
 
         if (li1!=choice1){
-            totol-=20;
+            totol-=10;
             i++;
         }
-        if (li2!=choice2){
-            totol-=20;
+         if (li2!=choice2){
+            totol-=10;
             i++;
         }
-        if (li3!=choice3){
-            totol-=20;
+         if (li3!=choice3){
+            totol-=10;
             i++;
         }
-        if (li4!=choice4){
-            totol-=20;
+         if (li4!=choice4){
+            totol-=10;
             i++;
         }
-        if (li5!=choice5){
-            totol-=20;
+         if (li5!=choice5){
+            totol-=10;
+            i++;
+        }
+		if (li6!=choice6){
+            totol-=10;
+            i++;
+        }
+		if (li7!=choice7){
+            totol-=10;
+            i++;
+        }
+		if (li8!=choice8){
+            totol-=10;
+            i++;
+        }
+		if (li9!=choice9){
+            totol-=10;
+            i++;
+        }
+		if (li10!=choice10){
+            totol-=10;
             i++;
         }
 
